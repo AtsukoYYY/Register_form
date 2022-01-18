3836bf2efdc47a95e42beee98e316b42b3fbce66
Atsuko Yamada <60997895+AtsukoYYY@users.noreply.github.com>
2022-01-18 21:54:08 +0900

    initial commit

--- src/compornents/InputField.tsx
+++ src/compornents/InputField.tsx
@@ -5,13 +5,48 @@ import Alert from "@mui/material/Alert";
 
 const InputField = () => {
   const [inputUserName, setInputUserName] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const [inputPasswordConfirm, setInputPasswordConfirm] = useState("");
+
   const handleChangeUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputUserName(event.target.value);
+  };
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputEmail(event.target.value);
+  };
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPassword(event.target.value);
+  };
+  const handleChangePasswordConfirm = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputPasswordConfirm(event.target.value);
   };
   const [successAlertFlag, setSuccessAlertFlag] = useState(false);
   const [errorAlertFlag, setErrorAlertFlag] = useState(false);
   const ClickRegister = () => {
     if (inputUserName.length <= 0) {
+      setSuccessAlertFlag(false);
+      setErrorAlertFlag(true);
+      return;
+    }
+    if (inputEmail.length <= 0) {
+      setSuccessAlertFlag(false);
+      setErrorAlertFlag(true);
+      return;
+    }
+    if (inputPassword.length <= 0) {
+      setSuccessAlertFlag(false);
+      setErrorAlertFlag(true);
+      return;
+    }
+    if (inputPasswordConfirm.length <= 0) {
+      setSuccessAlertFlag(false);
+      setErrorAlertFlag(true);
+      return;
+    }
+    if (inputPassword !== inputPasswordConfirm) {
       setSuccessAlertFlag(false);
       setErrorAlertFlag(true);
       return;
@@ -33,16 +68,32 @@ const InputField = () => {
         />
       </div>
       <div>
-        <TextField sx={{ m: 1 }} id="outlined-basic" label="email" />
+        <TextField
+          sx={{ m: 1 }}
+          id="outlined-basic"
+          label="email"
+          value={inputEmail}
+          onChange={handleChangeEmail}
+        />
       </div>
       <div>
-        <TextField sx={{ m: 1 }} id="outlined-basic" label="password" />
+        <TextField
+          sx={{ m: 1 }}
+          id="outlined-basic"
+          label="password"
+          type="password"
+          value={inputPassword}
+          onChange={handleChangePassword}
+        />
       </div>
       <div>
         <TextField
           sx={{ m: 1 }}
           id="outlined-basic"
           label="password confirm "
+          type="password"
+          value={inputPasswordConfirm}
+          onChange={handleChangePasswordConfirm}
         />
       </div>
       <Button onClick={ClickRegister} sx={{ m: 1 }} variant="contained">
